60c87b562c07d36a227e52618e30b51a5a143195
Mrmarfik <mrmarfik@gmail.com>
2021-04-04 13:10:19 +0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+$(function() {
+	$('.carousel-inner').slick({
+		speed: 1500,
+		dots:true,
+		adaptiveHeight: true,
+		prevArrow: '<button type="button" class="slick-prev"><img src="img/right-arrow (1).svg"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="img/right-arrow (2).svg"></button>',  
+	});
+})
